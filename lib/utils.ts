5ac97d9e2811e232d0c1fbd9a7f48a9332438fd8
phanthebao1234 <phanthebao888@gmail.com>
2024-07-12 09:54:43 +0700

    Sign in & Sign Up UI

--- lib/utils.ts
+++ lib/utils.ts
@@ -209,3 +209,15 @@ export const authFormSchema = (type: string) => z.object({
   email: z.string().email(),
   password: z.string().min(8),
 })
+// export const authFormSchema = z.object({
+//   firstName: z.string().min(3),
+//   lastName: z.string().min(3),
+//   address1: z.string().max(50),
+//   city: z.string().max(50),
+//   state: z.string().min(2).max(2),
+//   postalCode: z.string().min(3).max(6),
+//   dateOfBirth: z.string().min(3),
+//   ssn: z.string().min(3),
+//   email: z.string().email(),
+//   password: z.string().min(8),
+// })
