4ae868a0b13960c9c2c0db2c3aa27629cbed5409
phanthebao1234 <phanthebao888@gmail.com>
2024-07-24 23:58:19 +0700

    update user.actions.ts signup

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -37,7 +37,21 @@ const AuthForm = ({ type }: { type: string }) => {
         
         try {
             if(type === 'sign-up') {
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+                }
+                
                 const newUser = await signUp(data)
+                
                 setUser(newUser)
                 
                 if(newUser) router.push('/')
@@ -87,11 +101,11 @@ const AuthForm = ({ type }: { type: string }) => {
                     </p>
                 </div>
             </header>
-            {/* {user ? ( */}
+            {user ? (
                 <div className="flex flex-col gap-4">
                     <PlaidLink user={user} variant="primary" />
                 </div>
-            {/* ) : ( */}
+             ) : ( 
                 <div>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -186,7 +200,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </form>
                     </Form>
                 </div>
-            {/* )} */}
+            )}
         </section>
     )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,7 +3,7 @@
 import { cookies } from "next/headers";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { ID } from "node-appwrite";
-import { encryptId, parseStringify } from "../utils";
+import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
 import {
   CountryCode,
   ProcessorTokenCreateRequest,
@@ -11,7 +11,7 @@ import {
   Products,
 } from "plaid";
 import { plaidClient } from "../plaid";
-import { addFundingSource } from "./../actions/dwolla.actions";
+import { addFundingSource, createDwollaCustomer } from "./../actions/dwolla.actions";
 import { revalidatePath } from "next/cache";
 
 const {
@@ -38,18 +38,44 @@ export const signIn = async ({ email, password }: signInProps) => {
   }
 };
 
-export const signUp = async (userData: SignUpParams) => {
-  const { email, password, firstName, lastName } = userData;
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
+  const { email, firstName, lastName } = userData;
+  
+  let newUserAccount;
 
   try {
-    const { account } = await createAdminClient();
+    const { account, database } = await createAdminClient();
 
-    const newUserAccount = await account.create(
-      ID.unique(),
-      email,
-      password,
+    newUserAccount = await account.create(
+      ID.unique(), 
+      email, 
+      password, 
       `${firstName} ${lastName}`
     );
+
+    if(!newUserAccount) throw new Error('Error creating user')
+
+    const dwollaCustomerUrl = await createDwollaCustomer({
+      ...userData,
+      type: 'personal'
+    })
+
+    if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
+
+    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+
+    const newUser = await database.createDocument(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...userData,
+        userId: newUserAccount.$id,
+        dwollaCustomerId,
+        dwollaCustomerUrl
+      }
+    )
+
     const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
@@ -59,12 +85,11 @@ export const signUp = async (userData: SignUpParams) => {
       secure: true,
     });
 
-    return parseStringify(newUserAccount);
+    return parseStringify(newUser);
   } catch (error) {
-    console.error("Error", error);
+    console.error('Error', error);
   }
-};
-// ... your initilization functions
+}
 
 export async function getLoggedInUser() {
   try {
@@ -130,8 +155,9 @@ export const createBankAccount = async ({
         shareableId,
       }
     )
+    return parseStringify(bankAccount)
   } catch (error) {
-    
+    console.error('Error',error)
   }
 }
 
